1c19f8cc65ec47cabc3f798203dd1c89cf4a0be7
MelaniePavez <me.pavez@duocuc.cl>
2024-11-25 20:57:20 -0300

    cambios
    cambios

--- src/app/tabs/consejos/consejos.page.ts
+++ src/app/tabs/consejos/consejos.page.ts
@@ -11,17 +11,17 @@ export class ConsejosPage {
     {
       nombre: 'Limpieza diaria',
       descripcion: 'Usa un limpiador suave adecuado para tu tipo de piel (seca, grasa, mixta o sensible). Limpia tu rostro por la mañana y por la noche para eliminar suciedad, impurezas y exceso de grasa..',
-      imagen: '/assets/img/consejos/woman-ongoing-gua-sha-face-procedure.jpg',
+      imagen: '/assets/img/consejos/consejo1.jpg',
     },
     {
       nombre: 'Primero la crema hidratante y luego el bloqueador',
       descripcion: 'La crema hidratante se debe aplicar primero para asegurar que tu piel esté bien hidratada y nutrida. El bloqueador solar debe aplicarse después de la crema hidratante, ya que actúa como una barrera protectora contra los daños del sol',
-      imagen: '/assets/img/consejos/young-lady-crossing-arms-while-pointing-up-white-bathrobe-towel-looking-confised-front-view.jpg',
+      imagen: '/assets/img/consejos/consejo2.jpg',
     },
     {
       nombre: '¿Cómo debo maquillarme?',
       descripcion: '1. Preparación de la piel 2. Base 3. Polvo 4. Contorno y rubor 5.Ojos 6. Cejas 7. Labios 8. Fijador',
-      imagen: '/assets/img/consejos/impressed-young-pretty-girl-sitting-makeup-table-with-makeup-tools-holding-chat-bubble-eyeshadow-brush-looking-brush-isolated-crimson-background.jpg',
+      imagen: '/assets/img/consejos/consejo3.jpg',
     },
     // Agregar más consejos aquí
   ];
